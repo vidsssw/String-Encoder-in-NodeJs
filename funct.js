f03aa26a60dc11e7b292aae8f94044f53c2ac974
vidsssw <51160533+vidsssw@users.noreply.github.com>
2019-11-13 19:50:47 +0530

    Add files via upload

--- funct.js
+++ funct.js
@@ -1,6 +1,7 @@
 module.exports.encode=encode
 
-function encode(str){
+function encode(str)
+{
   var c="abcdefghijklmnopqrstuvwxyz";
   var index=[]
 
@@ -9,7 +10,9 @@ function encode(str){
   var r=0;
   for (i of str)
   {
+
     r=r*c.length+c.indexOf(i)
+
   }
   r=String(r)
 
@@ -17,28 +20,32 @@ function encode(str){
   var count=0
 
   var t=0
+
   for(i=1;i<=Math.ceil(r.length/2);i++)
   {
-  if(t==r.length-1)
+  if(t==r.length)
   {
     var no=Number(r.charAt(t))
   }
   else {
     var no=Number(r.substring(t,t+2))
+
   }
     t+=2
 
     if(no<=32)
     {
-      index.push(count)
-      no=no+32
-      ans=ans+String.fromCharCode(no)
+        index.push(count)
+        no=no+33
+        ans=ans+String.fromCharCode(no)
     }
     else {
       ans=ans+String.fromCharCode(no)
     }
 
+
     count=count+1
+
   }
 
   anslist.push(ans)
@@ -49,34 +56,44 @@ function encode(str){
 
 }
 
-function decode(str,index){
+function decode(str,index)
+{
 
   var c="abcdefghijklmnopqrstuvwxyz";
   var  no=""
   var count=0
   for(i of str)
   {
+
      if(index.indexOf(count)==-1)
-    {
+     {
        n=str.charCodeAt(str.indexOf(i))
        no+=String(n)
      }
      else {
         n=str.charCodeAt(str.indexOf(i))
-        n=Number(n)-32
-        no+=String(n)
+        n=Number(n)-33
+        if (n==0)
+        {
+            no+="00"
+        }
+        else {
+            no+=String(n)
+        }
+
      }
 
      count=count+1
   }
   var r=""
-
   no=Number(no)
 
   while (no!=0)
   {
-    r=r+c.charAt(no%c.length)
-    no=Math.floor(no/c.length)
+    console.log(no)
+    r=r+c.charAt(no%26)
+    console.log(r)
+    no=Math.floor(no/26)
   }
   if(r=="")
   {
@@ -84,3 +101,6 @@ function decode(str,index){
   }
    return r.split("").reverse().join("");
 }
+klist=encode("helloworldda")
+console.log(klist[3])
+console.log(String.fromCharCode(5))
